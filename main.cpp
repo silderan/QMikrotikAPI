a75dbb3dd1dc0d57b4a19d488201115524e30550
Silderan <silderan@gmail.com>
2015-03-28 21:01:48 +0100

    cleaning
    non commited main.cpp file

--- main.cpp
+++ main.cpp
@@ -21,12 +21,11 @@
 #include "QMikAPIExample.h"
 #include <QApplication>
 
-#include "Comm.h"
-
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
     QMikAPIExample w;
+
 	w.show();
 
     return a.exec();
